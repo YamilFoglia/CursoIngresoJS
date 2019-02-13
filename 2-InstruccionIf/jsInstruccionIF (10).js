6f6b0d0454086c3bbc417a76ae4d1046efd1483e
YamilFoglia <47358921+YamilFoglia@users.noreply.github.com>
2019-02-13 12:13:25 -0300

    commit if
    9 y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,30 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+
+	var numeroRandom;
+
+	numeroRandom=Math.floor((Math.random() * 10) + 1);
+
+	if (numeroRandom<11 && numeroRandom>8) 
+	{	
+		alert("Su nota es "+numeroRandom+", Excelente");
+	}else
+	{	
+		if (numeroRandom<9 && numeroRandom>3) 
+			{	
+				alert("Su nota es "+numeroRandom+", Aprobo");
+			}else
+			{
+				if (numeroRandom<4) 
+				{
+					alert("Su nota es "+numeroRandom+", Vamos, la proxima se puede");
+				}
+			}
+					
+
+
+	}
+			
 
 }//FIN DE LA FUNCIÓN
